17ac26a7eea81ded7e90042eead9b4b04294a532
SaiSabale04 <saisabale450@gmail.com>
2026-05-22 07:40:29 +0530

    adding extra content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 rent = int(input("Enter your flat rent = "))
 food = int(input("Enter the amount of food ordered ="))
+water_bill = int(input("Enter the amount of water bill = "))
 electricity_spend =int(input("Enter the total of electricity spend ="))
 charge_per_unit = int(input("Enter the charge per unit = "))
 persons = int(input("Enter the number of persons living in flat = "))
